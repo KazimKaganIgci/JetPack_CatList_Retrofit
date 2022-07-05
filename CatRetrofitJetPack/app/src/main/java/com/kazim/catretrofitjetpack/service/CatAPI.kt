fedd164f93a6b8fec169697920e8e0b766772b99
Kazım Kağan İğci <61113947+KazimKaganIgci@users.noreply.github.com>
2022-07-05 18:52:45 +0300

    Update CatAPI.kt

--- CatRetrofitJetPack/app/src/main/java/com/kazim/catretrofitjetpack/service/CatAPI.kt
+++ CatRetrofitJetPack/app/src/main/java/com/kazim/catretrofitjetpack/service/CatAPI.kt
@@ -5,6 +5,6 @@ import retrofit2.Call
 import retrofit2.http.GET
 
 interface CatAPI {
-    @GET("v1/breeds?attach_breed=2a7ba349-a4ac-4ab6-9e8f-d79901153f3d")
+    @GET("v1/breeds?attach_breed=2a7ba349-a4ac-4ab6-9e8f-?????????")
     fun  getData():Call<List<CatModelItem>>
 }
